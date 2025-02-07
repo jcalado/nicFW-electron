bd44637b9bbee8f992079c65ee1e78de81b96405
Joel Calado <joelcalado@gmail.com>
2025-02-07 16:01:37 +0000

    Add IApi interface to preload and update global window.api type

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,20 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+export interface IApi {
+  loadPreferences: () => Promise<void>,
+  getSerialPorts: () => Promise<void>,
+  connectPort: (port) => Promise<void>,
+  disconnectPort: (port) => Promise<void>,
+  readChannels: (port) => Promise<void>,
+  readBandPlan: (port) => Promise<void>,
+  readGroups: (port) => Promise<void>,
+  writeChannel: (channelNumber, channelData) => Promise<void>,
+  flashFirmware: (filePath) => Promise<void>,
+}
+
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    api: IApi
   }
 }
