4bd695299efdbb4747028ac2d29c2895b2f0cd2c
Joel Calado <joelcalado@gmail.com>
2025-02-11 17:34:55 +0000

    Enhance Firmware component: add firmware flashing functionality, improve version display, and implement firmware archive table

--- src/renderer/src/components/Firmware.tsx
+++ src/renderer/src/components/Firmware.tsx
@@ -1,27 +1,82 @@
-import { Toolbar, ToolbarButton } from '@fluentui/react-components'
-import { ArrowDownloadRegular } from '@fluentui/react-icons'
+import {
+  Button,
+  Spinner,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  Toolbar,
+  ToolbarButton
+} from '@fluentui/react-components'
+import { ArrowDownloadRegular, UsbPlugRegular } from '@fluentui/react-icons'
 import { makeStyles, typographyStyles } from '@fluentui/react-components'
 import { useEffect, useState } from 'react'
 
 const useStyles = makeStyles({
   header: typographyStyles.title2,
-  subheader: typographyStyles.subtitle1
+  subheader: {
+    ...typographyStyles.subtitle1,
+    marginTop: '0px'
+  },
+  archiveHeader: {
+    ...typographyStyles.subtitle2,
+    marginTop: '40px'
+  }
 })
 
 function Firmware({ isConnected }) {
-  const [version, setVersion] = useState('')
+  const [version, setVersion] = useState({ version: '', releaseType: '' })
+  const [isLoading, setIsLoading] = useState(true)
+  const [releaseType, setReleaseType] = useState('')
+  const [archive, setArchive] = useState([''])
+  const [selectedFirmware, setSelectedFirmware] = useState('')
 
   const downloadLatest = async () => {
-    const result = await window.api.getLatestFirmware()
+    const { version, firmwareLink, releaseType } = await window.api.getLatestFirmware()
+    console.log(version)
+  }
+
+  const flashFirmware = async () => {
+    console.log('Flashing firmware')
+    // First open a confirmation dialog
+    const result = await window.dialog.showMessageBox({
+      type: 'question',
+      title: 'Flash firmware',
+      message: `Are you sure you want to flash ${selectedFirmware}?`,
+      buttons: ['Yes', 'No']
+    })
     console.log(result)
   }
 
   useEffect(() => {
     console.log('Fetching latest version')
-    window.api.getLatestVersion().then(setVersion)
+    window.api.getLatestVersion().then(({ version, releaseType }) => {
+      console.log(version)
+      console.log(releaseType)
+
+      setVersion({ version, releaseType })
+
+      setIsLoading(false)
+    })
+  }, [])
+
+  useEffect(() => {
+    window.api.getFirmwareArchive().then(setArchive)
   }, [])
 
   const styles = useStyles()
+
+  const columns = [
+    { columnKey: 'file', label: 'File' },
+    { columnKey: 'created', label: 'Created At' },
+    { columnKey: 'options', label: '' }
+  ]
+
+  const handleFlash = async (item) => {
+    setSelectedFirmware(item)
+  }
 
   return (
     <div>
@@ -34,9 +89,48 @@ function Firmware({ isConnected }) {
         >
           Download
         </ToolbarButton>
+        <ToolbarButton
+          vertical
+          icon={<UsbPlugRegular />}
+          onClick={flashFirmware}
+          disabled={!isConnected && selectedFirmware === ''}
+        >
+          Flash
+        </ToolbarButton>
       </Toolbar>
-      <h1 className={styles.header}>Latest version</h1>
-      <h1 className={styles.subheader}>{version}</h1>
+      <h1 className={styles.header}>Latest nightly version</h1>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <h1 className={styles.subheader}>
+          {version.version} ({version.releaseType})
+        </h1>
+      )}
+
+      <h1 className={styles.archiveHeader}>Archive</h1>
+
+      <Table>
+        <TableHeader>
+          {columns.map((column) => (
+            <TableHeaderCell key={column.columnKey}>{column.label}</TableHeaderCell>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {archive &&
+            archive.length > 0 &&
+            archive.map((item) => (
+              <TableRow key={item} onClick={() => console.log('clicked')}>
+                <TableCell>{item}</TableCell>
+                <TableCell>{item}</TableCell>
+                <TableCell>
+                  <Button size="small" onClick={() => handleFlash(item)}>
+                    Flash
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
