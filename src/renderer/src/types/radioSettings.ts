a3cf4e4184e92787676dfa0785af80f24fbc08d3
Joel Calado <joelcalado@gmail.com>
2025-02-14 18:53:26 +0000

    Add new options for pin actions, status bar styles, battery styles, IF frequencies, and key tones in radio settings

--- src/renderer/src/types/radioSettings.ts
+++ src/renderer/src/types/radioSettings.ts
@@ -30,6 +30,42 @@ const ASLOptions = {
   "Inverted COS": 3
 }
 
+const PinActions = {
+  "Off": 0,
+  "Unlock": 1,
+  "Power Off": 2,
+}
+
+const SBarOptions = {
+  "Segment": 0,
+  "Stepped": 1,
+  "Solid": 2
+}
+
+const BattOptions = {
+  "Off": 0,
+  "Icon": 1,
+  "Percent": 2,
+  "Voltage": 3
+}
+
+const IfOptions = {
+  "8.46": 0,
+  "7.25": 1,
+  "6.35": 2,
+  "5.64": 3,
+  "5.08": 4,
+  "4.62": 5,
+  "4.23": 6
+}
+
+const KeytoneOptions =  {
+  "Off": 0,
+  "On": 1,
+  "Differential":2 ,
+  "Voice": 3
+}
+
 type RadioSettings = {
   magic: number
   squelch: number
@@ -44,7 +80,7 @@ type RadioSettings = {
   micGain: number
   txDeviation: number
   xtal671: number
-  battStyle: number
+  battStyle: typeof BattOptions
   scanRange: number
   scanPersist: number
   scanResume: number
@@ -60,10 +96,10 @@ type RadioSettings = {
   keyLock: number
   bluetooth: number
   powerSave: number
-  keyTones: number
+  keyTones: typeof KeytoneOptions
   ste: number
   rfGain: number
-  sBarStyle: number
+  sBarStyle: typeof SBarOptions
   sqNoiseLev: number
   lastFmtFreq: number
   vox: number
@@ -76,13 +112,22 @@ type RadioSettings = {
   asl: typeof ASLOptions
   disableFmt: number
   pin: number
-  pinAction: number
+  pinAction: typeof PinActions
   lcdInverted: number
   afFilters: typeof AfFilters
-  ifFreq: number
+  ifFreq: typeof IfOptions
   sBarAlwaysOn: number
   filler: Buffer
 }
 
-export { AfFilters, PttOptions, ASLOptions }
+export {
+  AfFilters,
+  ASLOptions,
+  BattOptions,
+  IfOptions,
+  KeytoneOptions,
+  PttOptions,
+  PinActions,
+  SBarOptions
+}
 export type { RadioSettings, VfoState }
