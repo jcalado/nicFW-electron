837f106a0997773ec54bdb552f035506412f43f5
Joel Calado <joelcalado@gmail.com>
2025-03-03 08:39:48 +0000

    Add ScanPreset and RadioSettings types to index for improved type management

--- src/renderer/src/types/index.ts
+++ src/renderer/src/types/index.ts
@@ -1,5 +1,7 @@
 import Band from './band'
 import Group from './group'
 import Channel from './channel'
+import ScanPreset from './scanPreset'
+import { RadioSettings } from './radioSettings'
 
-export type { Band, Group, Channel }
+export type { Band, Group, Channel, ScanPreset, RadioSettings }
